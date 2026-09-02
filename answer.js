b2a58175a120d56f6d55ec40b1ba72675bb6fdef
Galib Mehboob <86629449+Galibmehboob@users.noreply.github.com>
2026-09-02 20:53:33 +0600

    forth problemagain solve

--- answer.js
+++ answer.js
@@ -40,3 +40,24 @@ function validateUsername(username) {
 }
 // console.log(validateUsername("galib@gmail.com"));
 
+
+
+
+// Ques 4
+
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+    let fare = 50;
+
+    if (distance > 2) {
+        fare = fare + (distance - 2) * 15;
+    }
+
+    fare = fare + waitingMinutes * 2;
+
+    if (isNight) {
+        fare = fare * 1.2;
+    }
+
+    return fare;
+}
+
