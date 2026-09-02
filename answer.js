18528f5d6ae58eae3e9fee5eb8c0ed7aff40a0fb
Galib Mehboob <86629449+Galibmehboob@users.noreply.github.com>
2026-09-02 20:49:09 +0600

    second problem solve

--- answer.js
+++ answer.js
@@ -3,3 +3,24 @@
 function describeValue(value) {
     return `${typeof value} - ${value ? "truthy" : "falsy"}`;
 }
+//Ques 2
+
+function getDayType(day) {
+    switch (day.toLowerCase()) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+
+        default:
+            return "Invalid Day";
+    }
+}
+
+console.log(getDayType("saturday"));
